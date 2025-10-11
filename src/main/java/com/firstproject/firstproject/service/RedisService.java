02ea6581d7e895e76c450f479a6b366721982bdf
divysoni03 <chetanshudha267@gmail.com>
2025-10-11 23:57:21 +0530

    removed all bugs from RedisService.java

--- src/main/java/com/firstproject/firstproject/service/RedisService.java
+++ src/main/java/com/firstproject/firstproject/service/RedisService.java
@@ -1,7 +1,6 @@
 package com.firstproject.firstproject.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.firstproject.firstproject.api.QuoteObj;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -14,28 +13,27 @@ import java.util.concurrent.TimeUnit;
 public class RedisService {
 
     @Autowired
-    private RedisTemplate redisTemplate;
+    private RedisTemplate<String, Object> redisTemplate;
 
-    public <T> T get(String key,Class<T> entityClass) {
-        try{
+    public <T> T get(String key, Class<T> entityClass) {
+        try {
             Object o = redisTemplate.opsForValue().get(key);
+            if (o == null) return null;
             ObjectMapper mapper = new ObjectMapper();
-            mapper.readValue(o.toString(), entityClass);
-        } catch(Exception e) {
-//            e.printStackTrace();
-            log.error("Exception: ", e);
+            return mapper.readValue(o.toString(), entityClass);
+        } catch (Exception e) {
+            log.error("Exception in get(): ", e);
             return null;
         }
-        return null;
     }
 
-    public void set(String key, Object o, Long ttl) { /* ttl - time to live */
-        try{
+    public void set(String key, Object o, Long ttl) { // ttl = time-to-live in seconds
+        try {
             ObjectMapper mapper = new ObjectMapper();
             String jsonValue = mapper.writeValueAsString(o); // converting the incoming object as string so the StringRedisSerializer can convert it to serialized string
-            redisTemplate.opsForValue().set(key, o, ttl, TimeUnit.SECONDS);
-        } catch(Exception e) {
-            log.error("Exception: ", e);
+            redisTemplate.opsForValue().set(key, jsonValue, ttl, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            log.error("Exception in set(): ", e);
         }
     }
 }
