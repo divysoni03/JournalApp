e729eb3c48e6870fd189a5b76b198b622a077260
divysoni03 <chetanshudha267@gmail.com>
2025-10-12 00:57:53 +0530

    kafka fallback protection

--- src/main/java/com/firstproject/firstproject/scheduler/UserScheduler.java
+++ src/main/java/com/firstproject/firstproject/scheduler/UserScheduler.java
@@ -3,10 +3,12 @@ package com.firstproject.firstproject.scheduler;
 import com.firstproject.firstproject.entity.Journal;
 import com.firstproject.firstproject.entity.User;
 import com.firstproject.firstproject.enums.Sentiment;
+import com.firstproject.firstproject.model.SentimentData;
 import com.firstproject.firstproject.repo.UserRepoImpl;
 import com.firstproject.firstproject.service.EmailService;
 import com.firstproject.firstproject.service.SentimentAnalysis;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -29,35 +31,72 @@ public class UserScheduler {
 //    @Autowired
 //    private SentimentAnalysis sentimentAnalysis;
 
+    @Autowired
+    private KafkaTemplate<String, SentimentData> kafkaTemplate;
+
     @Scheduled(cron = "0 */2 * * * *")
     public void fetchUserAndSendMail() {
+
         List<User> users = userRepo.getUsersForSA();
-        for(User user: users) {
+        for (User user : users) {
             List<Journal> journalEntries = user.getJournalEntries();
-
-            // filtering sentiments of the user of last 7 days, and then retrieving all the sentiments and putting them into this list
-            List<Sentiment> sentiments = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x->x.getSentiment()).collect(Collectors.toList());
-
-            // counting every sentiment of the user
-            Map<Sentiment, Integer> sentimentCount = new HashMap<>();
-            for(Sentiment sentiment: sentiments) {
-                if(sentiment != null)
-                    sentimentCount.put(sentiment, sentimentCount.getOrDefault(sentiment, 0) + 1);
+            List<Sentiment> sentiments = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x -> x.getSentiment()).collect(Collectors.toList());
+            Map<Sentiment, Integer> sentimentCounts = new HashMap<>();
+            for (Sentiment sentiment : sentiments) {
+                if (sentiment != null)
+                    sentimentCounts.put(sentiment, sentimentCounts.getOrDefault(sentiment, 0) + 1);
             }
-
-            // seeing which sentiment is max or frequent
             Sentiment mostFrequentSentiment = null;
             int maxCount = 0;
-            for(Map.Entry<Sentiment, Integer> entry : sentimentCount.entrySet()) {
+            for (Map.Entry<Sentiment, Integer> entry : sentimentCounts.entrySet()) {
                 if (entry.getValue() > maxCount) {
                     maxCount = entry.getValue();
                     mostFrequentSentiment = entry.getKey();
                 }
             }
-            if(mostFrequentSentiment != null) {
-                emailService.sendMail(user.getEmail(), "Sentiment of last 7 days", mostFrequentSentiment.toString());
+            if (mostFrequentSentiment != null) {
+                SentimentData sentimentData = SentimentData.builder().email(user.getEmail()).sentiment("Sentiment for last 7 days " + mostFrequentSentiment).build();
+                try{
+                    kafkaTemplate.send("weekly-sentiments", sentimentData.getEmail(), sentimentData);
+                }catch (Exception e){
+                    emailService.sendMail(sentimentData.getEmail(), "Sentiment for previous week", sentimentData.getSentiment());
+                }
             }
+            /* with not fallback protection if kafka doesn't work all will be fail
+            * if (mostFrequentSentiment != null) {
+                SentimentData sentimentData = SentimentData.builder().email(user.getEmail()).sentiment("Sentiment for last 7 days " + mostFrequentSentiment).build();
+                kafkaTemplate.send("weekly-sentiments", sentimentData.getEmail(), sentimentData);
+            }*/
         }
+
+        // simple but with no kafka
+//        List<User> users = userRepo.getUsersForSA();
+//        for(User user: users) {
+//            List<Journal> journalEntries = user.getJournalEntries();
+//
+//            // filtering sentiments of the user of last 7 days, and then retrieving all the sentiments and putting them into this list
+//            List<Sentiment> sentiments = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x->x.getSentiment()).collect(Collectors.toList());
+//
+//            // counting every sentiment of the user
+//            Map<Sentiment, Integer> sentimentCount = new HashMap<>();
+//            for(Sentiment sentiment: sentiments) {
+//                if(sentiment != null)
+//                    sentimentCount.put(sentiment, sentimentCount.getOrDefault(sentiment, 0) + 1);
+//            }
+//
+//            // seeing which sentiment is max or frequent
+//            Sentiment mostFrequentSentiment = null;
+//            int maxCount = 0;
+//            for(Map.Entry<Sentiment, Integer> entry : sentimentCount.entrySet()) {
+//                if (entry.getValue() > maxCount) {
+//                    maxCount = entry.getValue();
+//                    mostFrequentSentiment = entry.getKey();
+//                }
+//            }
+//            if(mostFrequentSentiment != null) {
+//                emailService.sendMail(user.getEmail(), "Sentiment of last 7 days", mostFrequentSentiment.toString());
+//            }
+//        }
 
 
 
--- src/main/java/com/firstproject/firstproject/service/QuoteService.java
+++ src/main/java/com/firstproject/firstproject/service/QuoteService.java
@@ -1,9 +1,8 @@
 package com.firstproject.firstproject.service;
 
-import com.firstproject.firstproject.api.QuoteObj;
+import com.firstproject.firstproject.model.QuoteObj;
 import com.firstproject.firstproject.cache.AppCache;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
